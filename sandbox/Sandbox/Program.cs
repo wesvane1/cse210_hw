c3ce73bcb2e5f403130e58a32a875198a4bb0a44
wesvane1 <wesleyvane13@gmail.com>
2023-01-06 09:23:32 -0700

    Finished Prep 1 Activity

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}.");
         Console.WriteLine("Hello Sandbox World!");
     }
 }
