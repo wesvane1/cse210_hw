58c0b1a132eb1f0b5e2635f589ab30714aeee685
wesvane1 <wesleyvane13@gmail.com>
2023-01-06 10:08:37 -0700

    Finished the C# prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade for this course? (input in percent): ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A in this class!");
+        }
+        if (grade >= 80)
+        {
+            Console.WriteLine("You got an B in this class!");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("You got an C in this class!");
+        }
+        if (grade >= 60)
+        {
+            Console.WriteLine("You got an D in this class. You may want to consider retaking the course");
+        }
+        if (grade < 60)
+        {
+            Console.WriteLine("You got an F in this class. You may want to consider retaking the course");
+        }
     }
 }
