c450d528506f5236221bfc5a6e09625dbb67867c
wesvane1 <wesleyvane13@gmail.com>
2023-01-06 09:28:27 -0700

    gramatical changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
         Console.Write("What is your first name? ");
-        string firstName=Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string lastName=Console.ReadLine();
+        string lastName = Console.ReadLine();
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
