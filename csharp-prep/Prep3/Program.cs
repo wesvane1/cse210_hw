232c95e47ed283c269e164edb2b0e1d5ccf57541
wesvane1 <wesleyvane13@gmail.com>
2023-01-09 10:07:57 -0700

    C# prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number?: ");
+        string rawMagicNum = Console.ReadLine();
+        int magicNum = int.Parse(rawMagicNum);
+        bool isGuessCorrect =false;
+        while(!isGuessCorrect)
+        {
+            Console.Write("What is your guess?: ");
+            string rawGuess = Console.ReadLine();
+            int guess = int.Parse(rawGuess);
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("try Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("try lower");
+            }
+            else
+            {
+                Console.WriteLine("Correct! Nice Job!");
+                isGuessCorrect=true;
+            }
+        }
+
     }
 }
