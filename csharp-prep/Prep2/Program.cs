66eee286324ad400814d84599ce8ac6420fa0c75
wesvane1 <wesleyvane13@gmail.com>
2023-01-09 07:45:10 -0700

    simple changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,34 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
-        Console.Write("What was your grade for this course? (input in percent): ");
-        string userInput = Console.ReadLine();
+        Console.Write("What was your grade in the class?: ");
+        string userInput=Console.ReadLine();
         int grade = int.Parse(userInput);
 
-        if (grade >= 90)
+        if (grade >=90)
         {
-            string letter= "A";
-            Console.WriteLine($"You got an {letter} in this class! You passed!");
+            string letter ="A";
+            Console.WriteLine($"You got an {letter} in the class! Congratulations!");
         }
-        else if (grade >= 80)
+        if (grade >=80)
         {
-            string letter= "B";
-            Console.WriteLine($"You got an {letter} in this class! You passed!");
+            string letter ="B";
+            Console.WriteLine($"You got an {letter} in the class!");
         }
-        else if (grade >= 70)
+        if (grade >=70)
         {
-            string letter= "C";
-            Console.WriteLine($"You got an {letter} in this class! You passed!");
+            string letter ="C";
+            Console.WriteLine($"You got an {letter} in the class!");
         }
-        else if (grade >= 60)
+        if (grade >=60)
         {
-            string letter= "D";
-            Console.WriteLine($"You got an {letter} in this class. You may want to consider retaking the course");
+            string letter ="D";
+            Console.WriteLine($"You got an {letter} in the class! You may want to consider retaking the course.");
         }
-        else
+        if (grade <60)
         {
-            string letter= "F";
-            Console.WriteLine($"You got an {letter} in this class. You may want to consider retaking the course");
+            string letter ="F";
+            Console.WriteLine($"You got an {letter} in the class! You may want to consider retaking the course.");
         }
+
     }
 }
