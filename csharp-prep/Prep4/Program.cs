39f1bf8ffd323a65f5022698858483cf523bb9aa
wesvane1 <wesleyvane13@gmail.com>
2023-01-10 15:43:16 -0700

    Finished C# prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        int number;
+        List<int> numberList = new List<int>();
+
         Console.WriteLine("Hello Prep4 World!");
+        
+        Console.WriteLine("Enter a list of numbers, then type 0 when you are done");
+        bool zero=false;
+        while(!zero)
+        {
+            Console.Write("Enter a number: ");
+            number=Convert.ToInt32(Console.ReadLine());
+            numberList.Add(number);
+
+            if (number==0)
+            {
+                zero=true;
+            }
+        }
+        // for (int i = 0; i<numbers.Count; i++)
+        // {
+        //     Console.WriteLine(numbers[i]);
+        // }
+        // This just prints out all the intigers in the list
+
+        int sumOfAllNumbers=numberList.Sum();
+        Console.WriteLine($"The sum of all numbers is: {sumOfAllNumbers}");
+
+
+        float averageOfAllNumbers = ((float)sumOfAllNumbers/(numberList.Count-1));
+        // int averageOfAllNumbers=sumOfAllNumbers/(numberList.Count-1);
+        Console.WriteLine($"The average of all numbers is: {averageOfAllNumbers}");
+
+        int largestNumber=numberList.Max();
+        Console.WriteLine($"The largest number in the list was {largestNumber}");
+
+
+
     }
 }
---
+++ csharp-prep/Prep4/disregard.py
@@ -0,0 +1,23 @@
+words=[]
+words.append("phone")
+words.append("keyboard")
+words.append("mouse")
+  # using System.Collections.Generic;
+  # List<string> words = new List<string>();
+  # words.Add("phone");
+  # words.Add("keyboard");
+  # words.Add("mouse");
+
+for i in range(len(words)):
+  print(words[i])
+    # for (int i = 0; i < words.Count; i++)
+    # {
+    #     Console.WriteLine(words[i]);
+    # }
+
+for word in words:
+  print(word)
+      # foreach (string word in words)
+      # {
+      #   Console.WriteLine(word);
+      # }
