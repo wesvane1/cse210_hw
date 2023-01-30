f90b40f3bb62b197ade77fa384d354de4706437a
wesvane1 <wesleyvane13@gmail.com>
2023-01-30 16:29:39 -0700

    Finished fraction program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+        Fraction frac2 = new Fraction(5);
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+        Fraction frac3 = new Fraction(3,4);
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
