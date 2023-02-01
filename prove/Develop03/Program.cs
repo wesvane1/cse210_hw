5e74770c4a2efdbbdf6b56b042ab7be1444c0916
wesvane1 <wesleyvane13@gmail.com>
2023-02-01 10:13:51 -0700

    started work on scirpture mastery

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        // This will start by displaying "AAA" and waiting for the user to press the enter key
+        Console.WriteLine("I am happy");
+        Console.WriteLine("Press the enter key to clear the text. NOT the power button");
+        Console.ReadLine();
+
+        // This will clear the console
+        Console.Clear();
+
+        // This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("The text was cleared");
     }
 }
