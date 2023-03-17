83a365709439af31cc460edc75d66bc4b8ed33a3
wesvane1 <wesleyvane13@gmail.com>
2023-03-17 09:58:02 -0600

    Checks to see if address is in the USA

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -5,4 +5,70 @@ public class Address
   private string _state;
   private int _zipCode;
   private string _country;
+  public Address(string streetAddress, string city, string state, int zipCode, string country)
+  {
+    _streetAddress = streetAddress;
+    _city = city;
+    _state = state;
+    _zipCode = zipCode;
+    _country = country;
+  }
+
+  public bool IsInUSA()
+  {
+    if (_country == "USA")
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  public string GetA1()
+  {
+    Console.WriteLine($"{_streetAddress} {_city}, {_state}, {_zipCode}, {_country}");
+    return $"{_streetAddress} {_city}, {_state}, {_zipCode}, {_country}";
+  }
+
+  // public string SetFullAddress()
+  // {
+  //   GetStreetAddress();
+  //   GetCity();
+  //   GetState();
+  //   GetZipCode();
+  //   GetCountry();
+  //   return ($"{_streetAddress} {_city}, {_state}, {_zipCode}, {_country}");
+  // }
+  // public string GetStreetAddress()
+  // {
+  //   Console.Write("What is the street you live on?: ");
+  //   string _streetAddress = Console.ReadLine();
+  //   return _streetAddress;
+  // }
+  // public string GetCity()
+  // {
+  //   Console.Write("What is the city you live in?: ");
+  //   string _city = Console.ReadLine();
+  //   return _city;
+  // }
+  // public string GetState()
+  // {
+  //   Console.Write("What is the state you live in?: ");
+  //   string _state = Console.ReadLine();
+  //   return _state;
+  // }
+  // public int GetZipCode()
+  // {
+  //   Console.Write("What is your zip code?: ");
+  //   int _zipCode = int.Parse(Console.ReadLine());
+  //   return _zipCode;
+  // }
+  // public string GetCountry()
+  // {
+  //   Console.Write("What country do you live in?: ");
+  //   string _country = Console.ReadLine();
+  //   return _country;
+  // }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
+    static List<Address> orderList = new List<Address>();
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+
+        // ! There is a bool set to check if the address is in the USA, this will be useful for the added shipping cost
+
+        Address a1 = new Address("3893 North Legacy Common Ave.", "Meridian", "Idaho", 83646, "USA");
+        Address a2 = new Address("3893 North Legacy Common Ave.", "Meridian", "Idaho", 83646, "CA");
+        // The line below prints if the address is in the USA or not.
+        Console.WriteLine(a1.IsInUSA());
+        Console.WriteLine(a2.IsInUSA());
     }
 }
