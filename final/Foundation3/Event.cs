0b05dec15f59c4d3766258001e8009ac7b13cba0
wesvane1 <wesleyvane13@gmail.com>
2023-03-18 12:57:16 -0600

    rearranged the protected variables

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -5,6 +5,6 @@ public class Event
   protected string _eventTitle;
   protected string _eventDescription;
   protected string _eventDate;
-  protected string _eventTime;
   protected string _eventAddress;
+  protected string _eventTime;
 }
