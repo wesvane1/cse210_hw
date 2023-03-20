9bbbde5d9dc499da59bcc30a93b6051b8777f682
wesvane1 <wesleyvane13@gmail.com>
2023-03-20 16:29:05 -0600

    added customer fname and lname

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,4 +1,11 @@
 public class Customer
 {
-  private string _custName;
+  private string _customerFName;
+  private string _customerLName;
+
+  public Customer(string customerFName, string customerLName)
+  {
+    _customerFName = customerFName;
+    _customerLName = customerLName;
+  }
 }
