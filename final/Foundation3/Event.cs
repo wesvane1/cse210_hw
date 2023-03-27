8d3c256bb2d748e9b717fe6b530c46037a42a8e2
wesvane1 <wesleyvane13@gmail.com>
2023-03-27 11:39:56 -0600

    added a constructor to make base work

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -7,4 +7,15 @@ public class Event
   protected string _eventDate;
   protected string _eventAddress;
   protected string _eventTime;
+  protected string _eventType;
+
+  public Event(string eventType, string eventTitle, string eventDescription, string eventDate, string eventTime, string eventAddress)
+  {
+    _eventType = eventType;
+    _eventTitle = eventTitle;
+    _eventDescription = eventDescription;
+    _eventDate = eventDate;
+    _eventTime = eventTime;
+    _eventAddress = eventAddress;
+  }
 }
