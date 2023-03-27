7dd5e0bd78520213ce7ce03fe910732d6640a565
wesvane1 <wesleyvane13@gmail.com>
2023-03-27 07:59:23 -0600

    moved the lecture instance here

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Lecture l1 = new Lecture("John Snow", 500, "An evening with John Snow", "NULL", "April 1, 2023", "6:00 PM", "123 street");
+        l1.GetStandardDetails();
     }
 }
