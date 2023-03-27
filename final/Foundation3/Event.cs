fe494865bb5b71ca5404e01d284b22fa29f514ff
wesvane1 <wesleyvane13@gmail.com>
2023-03-27 07:59:14 -0600

    removed the lecture instance

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -7,6 +7,4 @@ public class Event
   protected string _eventDate;
   protected string _eventAddress;
   protected string _eventTime;
-
-  Lecture l1 = new Lecture("John Snow", 500, "An evening with John Snow", "NULL", "April 1, 2023", "6:00 PM", "123 street");
 }
